5fd21fcefa4b71c73d8acbe9f5c4f3b186f849db
Jennifer Graul <jennifer.graul@wobcom.de>
2023-06-26 16:21:10 +0200

    client: auto-format code

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,11 @@
-use std::process::Command;
 use std::error::Error;
+use std::process::Command;
 
-pub fn run(target: &String, port: u16, iperf3_params: Vec<String>) -> Result<(), Box<dyn Error>> {
+pub fn run(
+    target: &String,
+    port: u16,
+    iperf3_params: Vec<String>,
+) -> Result<(), Box<dyn Error>> {
     let mut iperf3_child = Command::new("iperf3")
         .arg("-c")
         .arg(target)
@@ -10,7 +14,9 @@ pub fn run(target: &String, port: u16, iperf3_params: Vec<String>) -> Result<(),
         .args(iperf3_params)
         .spawn()
         .expect("Failed to spawn iperf3 client");
-    
-    iperf3_child.wait().expect("Failed to wait for iperf3 client");
+
+    iperf3_child
+        .wait()
+        .expect("Failed to wait for iperf3 client");
     Ok(())
 }
